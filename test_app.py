9549ddce33bfa42700006adb60adfbe3156e00d9
Keith Olszewski <kwo20@msstate.edu>
2023-04-13 17:06:07 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -8,69 +8,69 @@ class TestBMICalculator(unittest.TestCase):
         original_input = mock.builtins.input
         #UNDER test (will return false)
         mock.builtins.input = lambda _: 0
-        self.assertTrue(height_inches_input() > 0 and height_inches_input() <= 12)
+        self.assertTrue(height_inches_input('0') > 0 and height_inches_input('0') <= 12)
         mock.builtins.input = original_input
         #MIN test
         mock.builtins.input = lambda _: 1
-        self.assertTrue(height_inches_input() > 0 and height_inches_input() <= 12)
+        self.assertTrue(height_inches_input('1') > 0 and height_inches_input('1') <= 12)
         mock.builtins.input = original_input
         #INTERIOR test
         mock.builtins.input = lambda _: 6
-        self.assertTrue(height_inches_input() > 0 and height_inches_input() <= 12)
+        self.assertTrue(height_inches_input('6') > 0 and height_inches_input('6') <= 12)
         mock.builtins.input = original_input
         #MAX test
         mock.builtins.input = lambda _: 12
-        self.assertTrue(height_inches_input() > 0 and height_inches_input() <= 12)
+        self.assertTrue(height_inches_input('12') > 0 and height_inches_input('12') <= 12)
         mock.builtins.input = original_input
         #OVER test (will return false)
         mock.builtins.input = lambda _: 13
-        self.assertTrue(height_feet_input() > 0 and height_feet_input() <= 12)
+        self.assertTrue(height_feet_input('13') > 0 and height_feet_input('13') <= 12)
         mock.builtins.input = original_input
 
   def test_height_feet_input(self):
         original_input = mock.builtins.input
         #UNDER test (will return false)
         mock.builtins.input = lambda _: 0
-        self.assertTrue(height_feet_input() > 0 and height_feet_input() <= 8)
+        self.assertTrue(height_feet_input('0') > 0 and height_feet_input('0') <= 8)
         mock.builtins.input = original_input
         #MIN test
         mock.builtins.input = lambda _: 1
-        self.assertTrue(height_feet_input() > 0 and height_feet_input() <= 8)
+        self.assertTrue(height_feet_input('1') > 0 and height_feet_input('1') <= 8)
         mock.builtins.input = original_input
         #INTERIOR test
         mock.builtins.input = lambda _: 6
-        self.assertTrue(height_feet_input() > 0 and height_feet_input() <= 8)
+        self.assertTrue(height_feet_input('6') > 0 and height_feet_input('6') <= 8)
         mock.builtins.input = original_input
         #MAX test
         mock.builtins.input = lambda _: 8
-        self.assertTrue(height_feet_input() > 0 and height_feet_input() <= 8)
+        self.assertTrue(height_feet_input('8') > 0 and height_feet_input('8') <= 8)
         mock.builtins.input = original_input
         #OVER test (will return false)
         mock.builtins.input = lambda _: 9
-        self.assertTrue(height_feet_input() > 0 and height_feet_input() <= 8)
+        self.assertTrue(height_feet_input('9') > 0 and height_feet_input('9') <= 8)
         mock.builtins.input = original_input
 
   def test_weight_input(self):
         original_input = mock.builtins.input
         #UNDER test (will return false)
         mock.builtins.input = lambda _: 0
-        self.assertTrue(weight_input() > 0 and weight_input() <= 1000)
+        self.assertTrue(weight_input('0') > 0 and weight_input('0') <= 1000)
         mock.builtins.input = original_input
         #MIN test
         mock.builtins.input = lambda _: 1
-        self.assertTrue(weight_input() > 0 and weight_input() <= 1000)
+        self.assertTrue(weight_input('1') > 0 and weight_input('1') <= 1000)
         mock.builtins.input = original_input
         #INTERIOR test
         mock.builtins.input = lambda _: 500
-        self.assertTrue(weight_input() > 0 and weight_input() <= 1000)
+        self.assertTrue(weight_input('500') > 0 and weight_input('500') <= 1000)
         mock.builtins.input = original_input
         #MAX test
         mock.builtins.input = lambda _: 1000
-        self.assertTrue(weight_input() > 0 and weight_input() <= 1000)
+        self.assertTrue(weight_input('1000') > 0 and weight_input('1000') <= 1000)
         mock.builtins.input = original_input
         #OVER test (will return false)
         mock.builtins.input = lambda _: 1001
-        self.assertTrue(weight_input() > 0 and weight_input() <= 1000)
+        self.assertTrue(weight_input('1001') > 0 and weight_input('1001') <= 1000)
         mock.builtins.input = original_input
     
   def test_bmi_calc(self):
